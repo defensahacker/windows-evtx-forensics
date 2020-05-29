45adb9ec08fe28be3678e22df2e4b771c253b1b5
Jacobo Avariento <23421429+spinfoo@users.noreply.github.com>
2020-05-29 20:21:30 +0200

    Create recursive_eID_evtx_TTPs_search.ps1

---
+++ recursive_eID_evtx_TTPs_search.ps1
@@ -0,0 +1,7 @@
+$eid= 51
+$file= "output_search_eid_$eid.txt"
+echo "*** Searching for events id: $eid ***" | Add-Content $file
+Get-ChildItem -R -Filter "*.evtx" -Name | Foreach-Object {
+	echo ">>>>>>> $_ <<<<<<<" | Add-Content -Path $file
+	python .\filter_eid_evtx.py "$_" $eid
+}
